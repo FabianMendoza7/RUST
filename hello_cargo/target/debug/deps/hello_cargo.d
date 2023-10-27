8d0716eddfed4bbfed9b78eedb41289617bd441e
unknown <fmendoza.ospina7@outlook.com>
2023-10-27 16:50:28 -0500

    Rust: Working with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\_Projects\RUST\hello_cargo\target\debug\hello_cargo.exe: C:\_Projects\RUST\hello_cargo\src\main.rs
