9098e01f103c56b4e0657d6e723af02fa7d28b5d
unknown <fmendoza.ospina7@outlook.com>
2023-10-26 20:32:41 -0500

    Rust: hello-world

---
+++ hello_cargo/target/debug/deps/hello_cargo-158ab6fde45fc4c7.d
@@ -0,0 +1,5 @@
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo-158ab6fde45fc4c7.rmeta: src\main.rs
+
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo-158ab6fde45fc4c7.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-7edb11be7a8d45ce.d
@@ -0,0 +1,5 @@
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.rmeta: src\main.rs
+
+C:\_Projects\RUST\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.d: src\main.rs
+
+src\main.rs:
