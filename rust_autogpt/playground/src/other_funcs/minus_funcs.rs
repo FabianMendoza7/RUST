0dd86e0c2eaa991df2d60666369d1dfb986fb5a8
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-12 13:41:40 -0500

    Course Udemy Rust

--- rust_autogpt/playground/src/other_funcs/minus_funcs.rs
+++ rust_autogpt/playground/src/other_funcs/minus_funcs.rs
@@ -2,3 +2,16 @@
 pub fn substract_ten(num: u32) -> u32 {
     num - 10
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn substract_ten_test(){
+        let x: u32 = 100;
+        let y: u32 = substract_ten(x);
+        println!("x and y are from test: {}, {}", x, y);
+        assert_eq!(y, 90);
+    }
+}
---
+++ rust_autogpt/playground/target/debug/deps/playground-948b8871cff2b2a8.d
@@ -0,0 +1,8 @@
+C:\_Projects\RUST\rust_autogpt\playground\target\debug\deps\playground-948b8871cff2b2a8.exe: src\main.rs src\my_funcs.rs src\other_funcs\mod.rs src\other_funcs\minus_funcs.rs
+
+C:\_Projects\RUST\rust_autogpt\playground\target\debug\deps\playground-948b8871cff2b2a8.d: src\main.rs src\my_funcs.rs src\other_funcs\mod.rs src\other_funcs\minus_funcs.rs
+
+src\main.rs:
+src\my_funcs.rs:
+src\other_funcs\mod.rs:
+src\other_funcs\minus_funcs.rs:
