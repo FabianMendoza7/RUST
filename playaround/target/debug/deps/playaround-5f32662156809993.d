550e4596e2329fb876eb45886633fa8d952fe1bc
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-03 07:07:43 -0500

    Creating Playaround project

---
+++ playaround/target/debug/deps/playaround-5f32662156809993.d
@@ -0,0 +1,5 @@
+C:\_Projects\rust\playaround\target\debug\deps\libplayaround-5f32662156809993.rmeta: src\main.rs
+
+C:\_Projects\rust\playaround\target\debug\deps\playaround-5f32662156809993.d: src\main.rs
+
+src\main.rs:
---
+++ playaround/target/debug/deps/playaround-8d9daf20839e0231.d
@@ -0,0 +1,5 @@
+C:\_Projects\rust\playaround\target\debug\deps\libplayaround-8d9daf20839e0231.rmeta: src\main.rs
+
+C:\_Projects\rust\playaround\target\debug\deps\playaround-8d9daf20839e0231.d: src\main.rs
+
+src\main.rs:
