c4b513f422d27b48af99698c7c63c873b6a99a1a
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-06 19:12:16 -0500

    Dynamic sized variables

--- playaround/src/main.rs
+++ playaround/src/main.rs
@@ -1,47 +1,61 @@
-const OUR_COURSE: &str = "Rust with AutoGPT";
+// const OUR_COURSE: &str = "Rust with AutoGPT";
 
 fn main() {
-    println!("Welcome to this course on {}!", OUR_COURSE);
+    // println!("Welcome to this course on {}!", OUR_COURSE);
 
-    // STACK
-    let x: i32;
-    x = 2;
-    println!("x is {}", x);
+    // // STACK
+    // let x: i32;
+    // x = 2;
+    // println!("x is {}", x);
 
-    let y: i32 = 4;
-    println!("y is {}", y);
+    // let y: i32 = 4;
+    // println!("y is {}", y);
 
-    // For Loop
-    for i in 0..=y {
-        if i != 4 {
-            print!("{}, ", i);
-            //print!("{}, \n", i);
-        } else {
-            println!("{}", i);
-        }
-    }
+    // // For Loop
+    // for i in 0..=y {
+    //     if i != 4 {
+    //         print!("{}, ", i);
+    //         //print!("{}, \n", i);
+    //     } else {
+    //         println!("{}", i);
+    //     }
+    // }
 
-    // Mutation
-    let mut z: i32 = 5;
-    print!("z was {} ", z);
-    z = 10;
-    println!("but is now {}", z);
+    // // Mutation
+    // let mut z: i32 = 5;
+    // print!("z was {} ", z);
+    // z = 10;
+    // println!("but is now {}", z);
 
-    let freezing_temp: f64 = -2.4;
-    println!("freezing_temp is {}", freezing_temp);
+    // let freezing_temp: f64 = -2.4;
+    // println!("freezing_temp is {}", freezing_temp);
 
-    let is_zero_reminder: bool = 10 % 4 != 0;
-    println!("is_zero_reminder is {}", is_zero_reminder);
+    // let is_zero_reminder: bool = 10 % 4 != 0;
+    // println!("is_zero_reminder is {}", is_zero_reminder);
 
-    let my_char: char = 'z';
-    println!("my_char is {}", my_char);
+    // let my_char: char = 'z';
+    // println!("my_char is {}", my_char);
 
-    let emoji_char: char = '😎';
-    println!("emoji_char is {}", emoji_char);
+    // let emoji_char: char = '😎';
+    // println!("emoji_char is {}", emoji_char);
 
-    let my_floats: [f32; 5] = [0.0; 5];
-    println!("my_floats is {:?}", my_floats);
+    // let my_floats: [f32; 5] = [0.0; 5];
+    // println!("my_floats is {:?}", my_floats);
 
-    let my_floats_new: [f32; 5] = my_floats.map(|n: f32| n + 2.0);
-    println!("my_floats_new is {:?}", my_floats_new);
+    // let my_floats_new: [f32; 5] = my_floats.map(|n: f32| n + 2.0);
+    // println!("my_floats_new is {:?}", my_floats_new);
+
+    // Dynamic sized variables.
+
+    // La siguiente variable es estática 
+    // y no se crea ni en el stack ni el heap, las cadenas
+    // literales tienen un tamaño conocido en tiempo de
+    // compilación y es inmutable.
+    let name: &str = "Shaun";
+    println!("name is {:?}", name);
+
+    //
+    let dynamic_name: String = String::from("Shaun McDonough");
+    println!("dynamic_name is {:?}", dynamic_name);
+    println!("my dynamic_name is stored in memory {:p}", &dynamic_name);
 }
